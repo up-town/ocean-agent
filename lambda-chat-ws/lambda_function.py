b7891973beca47ef3085532e7fb6ccc14188b647
Kyoungsu Park <kyopark2014@gmail.com>
2024-10-13 21:31:12 +0900

    update

--- lambda-chat-ws/lambda_function.py
+++ lambda-chat-ws/lambda_function.py
@@ -1697,7 +1697,7 @@ def get_documents_from_opensearch_for_subject_company(vectorstore_opensearch, qu
         query = query,
         k = top_k*2,  
         pre_filter={
-            "doc_level": {"$eq": "child"},
+            # "doc_level": {"$eq": "child"},
             "subject_company": {"$eq": subject_company}
         }
     )
@@ -1849,8 +1849,8 @@ def parallel_retriever(state: State):
             "payment"
         },
         {
-            "fleet",
-        }
+            "fleet"
+        },
         {
             "Infospectrum level",
             "Overall assessment"
