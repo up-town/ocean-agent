ed115b39431083c314a01da6b69275841821a428
Kyoungsu Park <kyopark2014@gmail.com>
2024-10-13 15:35:51 +0900

    update

--- lambda-chat-ws/lambda_function.py
+++ lambda-chat-ws/lambda_function.py
@@ -1991,7 +1991,7 @@ def run_agent_ocean(connectionId, requestId, query):
     )
     # print('response: ', response)
         
-    markdown_url = f"{path}{markdown_key}"
+    markdown_url = f"{path}{parse.quote(markdown_key)}"
     print('markdown_url: ', markdown_url)
         
     # html file
@@ -2009,15 +2009,10 @@ def run_agent_ocean(connectionId, requestId, query):
     )
     # print('response: ', response)
         
-    html_url = f"{path}{html_key}"
+    html_url = f"{path}{parse.quote(html_key)}"
     print('html_url: ', html_url)
     
-    encoded_html_url = parse.quote(html_url)
-    print('encoded html_url: ', encoded_html_url)
-    encoded_markdown_url = parse.quote(markdown_url)
-    print('encoded markdown_url: ', encoded_markdown_url)
-    
-    final_answer = final_doc+f"\n<a href={encoded_html_url} target=_blank>[미리보기 링크]</a>\n<a href={encoded_markdown_url} download=\"{subject_company}.md\">[다운로드 링크]</a>"    
+    final_answer = final_doc+f"\n<a href={html_url} target=_blank>[미리보기 링크]</a>\n<a href={markdown_url} download=\"{subject_company}.md\">[다운로드 링크]</a>"    
 
     return final_answer
 
