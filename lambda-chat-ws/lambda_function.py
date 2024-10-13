33c751121534296afd7a290ae47bf431238d63c1
Kyoungsu Park <kyopark2014@gmail.com>
2024-10-13 23:16:29 +0900

    update

--- lambda-chat-ws/lambda_function.py
+++ lambda-chat-ws/lambda_function.py
@@ -1700,13 +1700,32 @@ def get_documents_from_opensearch_for_subject_company(vectorstore_opensearch, qu
             ]
         }
     }
+    pre_filter={
+        'bool': {
+            'must': [
+                {
+                    'doc_level': {
+                        'query': 'parent',
+                        'fields': ['metadata.doc_level']
+                    }
+                },
+                {
+                    'simple_query_string': {
+                        'query': subject_company,
+                        'fields': ['metadata.subject_company']
+                    }
+                },
+            ],
+        }
+    }
     result = vectorstore_opensearch.similarity_search_with_score(
         query = query,
         k = top_k*2,  
-        pre_filter={
-            "doc_level": {"$eq": "parent"},
+        pre_filter=pre_filter
+       # {
+        #    "doc_level": {"$eq": "parent"},
             #"subject_company": {"$eq": subject_company}
-        }
+        #}
         #pre_filter={
             # "doc_level": {"$eq": "child"},
         #    "subject_company": {"$eq": subject_company}
@@ -1720,6 +1739,7 @@ def get_documents_from_opensearch_for_subject_company(vectorstore_opensearch, qu
         # "query": {
             # "match": {
                 # "director": "miller"}}})
+    
     
     filter =  {
         "bool":{
