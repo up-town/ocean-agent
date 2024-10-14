5f4b7ab670545058fcd3fd345ecc62be18c89568
Kyoungsu Park <kyopark2014@gmail.com>
2024-10-14 15:46:19 +0900

    update

--- lambda-chat-ws/lambda_function.py
+++ lambda-chat-ws/lambda_function.py
@@ -1704,6 +1704,7 @@ class State(TypedDict):
     subject_company: str
     rating_date: str    
     planning_steps: List[str]
+    sub_quries: List[List[str]]
     relevant_contexts : list[str]
     drafts : List[str]
     
@@ -1871,33 +1872,7 @@ def parallel_retriever(state: State):
     
     relevant_contexts = []    
     
-    sub_quries = [
-        {
-            "establish", 
-            "location", 
-            "management", 
-            "affiliated"
-        },
-        {
-            "cargo", 
-            "route", 
-            "owned/chartered", 
-            "strategy"
-        },
-        {
-            "financial performance", 
-            "route", 
-            "financial risk",
-            "payment"
-        },
-        {
-            "fleet"
-        },
-        {
-            "rating", #"infospectrum level"
-            "assessment" # overall assessment"
-        }        
-    ]
+    sub_quries = state["sub_quries"]
     
     for i, step in enumerate(planning_steps):
         print(f"{i}: {step}")
@@ -2044,6 +2019,34 @@ def run_agent_ocean(connectionId, requestId, query):
         "5. 종합 평가"
     ]
     
+    sub_quries = [
+        [
+            "establish", 
+            "location", 
+            "management", 
+            "affiliated"
+        ],
+        [
+            "cargo", 
+            "route", 
+            "owned/chartered", 
+            "strategy"
+        ],
+        [
+            "financial performance", 
+            "route", 
+            "financial risk",
+            "payment"
+        ],
+        [
+            "fleet"
+        ],
+        [
+            "rating", #"infospectrum level"
+            "assessment" # overall assessment"
+        ]        
+    ]
+    
     subject_company = query
     
     isTyping(connectionId, requestId, "")
@@ -2052,6 +2055,7 @@ def run_agent_ocean(connectionId, requestId, query):
     # Run the workflow
     inputs = {
         "subject_company": subject_company,
+        "sub_quries": sub_quries,
         "planning_steps": planning_steps
     }
     config = {
