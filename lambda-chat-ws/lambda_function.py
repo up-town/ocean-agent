dba1d2e0edc2fb970f90e9820ebf2c2bf7095ca8
Kyoungsu Park <kyopark2014@gmail.com>
2024-10-19 06:07:50 +0900

    update

--- lambda-chat-ws/lambda_function.py
+++ lambda-chat-ws/lambda_function.py
@@ -1763,7 +1763,10 @@ def get_documents_from_opensearch_for_subject_company(vectorstore_opensearch, qu
     boolean_filter = {
         "bool": {
             "should":[
-                {"match" : {"metadata.subject_company":subject_company}},
+                {
+                    "match" : {"metadata.subject_company":subject_company},
+                    "term": {"metadata.doc_level": "child"}
+                 }
             ]
         }
     }
